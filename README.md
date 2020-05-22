c838c61b83d1aee47bb5a47b54fa9cae03ba8d6f
@yeral-guerrero <gagl73@yahoo.com>
2020-05-23 01:43:40 +0200

    primer Foto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Plantilla_Front_2020
+Plantilla para proyectos de FrontEnd = HMLT5 + CSS3 + SASS + JAVASCRIPT
+# ObjetosAuto
---
+++ js/poo.js
@@ -0,0 +1,20 @@
+// dentro de los paréntesis se crean los parámetros de la función
+function coche(marca, modelo, anyo) 
+{
+	// la propiedad marca del objeto se le va asignar el valor que venga por parámetro
+	this.marca = marca;
+	this.modelo = modelo;
+	this.anyo = anyo;
+}
+
+// 'new' genera una nueva instancia de nuestra función constructora
+var cocheUno = new coche('Ford', 'C_HR', '2020');
+var cocheDos = new coche('Ferrari', 'Corsa', '2017');
+
+var varcoche = [];
+for (var i = 0; i<10;i++) 
+{
+    if(i%2==0)
+        varcoche[i] = cocheUno;
+    else varcoche[i] =cocheDos;
+}
